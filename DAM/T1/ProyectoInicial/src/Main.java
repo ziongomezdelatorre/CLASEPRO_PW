3a0932ea5a49fdcb4d9b1c773095c7ed935ac90a
Develop and System <developandsystem@gmail.com>
2025-09-30 19:54:15 +0200

    Update Main.java

--- DAM/T1/ProyectoInicial/src/Main.java
+++ DAM/T1/ProyectoInicial/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hola, este es mi primer programa en Java.\nEsta es la segunda linea\n");
+        System.out.print("Tercera linea");
+        System.out.print("Cuarta linea");
+    }
+
 }
+
+
