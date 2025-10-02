f4121f905f4aa8725635e60d8e3aae74e4673a12
Develop and System <developandsystem@gmail.com>
2025-10-02 19:49:59 +0200

    update

--- DAW/T1/ProyectoInicial/src/Main.java
+++ DAW/T1/ProyectoInicial/src/Main.java
@@ -1,4 +1,22 @@
 public class Main {
 
-    
+    // esto es un comentario
+    /*
+    este comentario
+    se puede dividir en
+    varias lineas de codigo
+     */
+    // TODO me falta esto
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la segunda linea del programa\n");
+        System.out.println("Este es mi primer programa en Java\n");
+    }
+
+    public void nombreMetodo() {
+
+    }
+
+    // TODO esto tambien esta pendiente
+
 }
