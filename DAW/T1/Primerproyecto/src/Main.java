6133827c3dc5285cea7ee94f4161ecd83298dd2d
ziongomezdelatorre <zion812005@gmail.com>
2025-10-03 14:55:40 +0200

    update

--- DAW/T1/Primerproyecto/src/Main.java
+++ DAW/T1/Primerproyecto/src/Main.java
@@ -1,2 +1,15 @@
-public class Main {
+public class Main  {
+
+    public static void main(String[] args) {
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Esta es mi segunda linea del programa");
+
+
+
+
+    }
+    public void nombremetodo(){
+
+    }
+
 }
